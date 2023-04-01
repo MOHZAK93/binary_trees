2faad0ce78b6b793727b1cf460e14d8bbb2c8714
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-04-01 09:16:47 +0000

    fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,13 +12,30 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int b1 = 0, b2 = 0;
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+
+	return (0);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ *
+ * @tree: pointer to the root node of the tree to measure height
+ *
+ * Return: height
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
 
 	if (tree)
 	{
-		b1 = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
-		b2 = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
-		return (b1 - b2);
+		size_t h1 = 0, h2 = 0;
+
+		h1 = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		h2 = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((h1 > h2) ? h1 : h2);
 	}
 	return (0);
 }
